cff326d1c96373a8127e037420a2ddf5c5af36a9
김병오 <byeongoh.kim@newin.co.kr>
2021-05-07 16:08:01 +0900

    2-2 완료

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+
 import Home from "Routes/Home";
 import Detail from "Routes/Detail";
 import Search from "Routes/Search";
@@ -8,10 +9,14 @@ import TV from "Routes/TV";
 export default () => {
     return (
         <Router>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/detail" component={Detail}></Route>
-            <Route path="/search" component={Search}></Route>
-            <Route path="/tv" component={TV}></Route>
+            <Switch>
+                <Route path="/" exact component={Home} />
+                <Route path="/detail" exact component={Detail} />
+                <Route path="/search" exact component={Search} />
+                <Route path="/tv" exact component={TV} />
+                <Route path="/tv/popular" render={() => <h1>popular</h1>} />
+                <Redirect from="*" to="/" />
+            </Switch>
         </Router>
     )
 }
