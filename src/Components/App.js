54fcfb01ff5dcaae0c50d688ec8ba49794d13429
김병오 <byeongoh.kim@newin.co.kr>
2021-05-07 16:59:17 +0900

    emotion 사용해서 스타일 세팅/ 작업 중

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,56 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
 import React, { Component } from "react";
 import Router from "Components/Router";
 
+import { Global, jsx, css } from "@emotion/react";
+
+import emotionReset from "emotion-reset";
+
 import Header from "Components/Header";
+
+import WebFont from "webfontloader";
+
+WebFont.load({
+  custom: {
+    families: ["Noto Sans KR"],
+    urls: [
+      "https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;700&display=swap",
+    ],
+  },
+});
+
+const GlobalStyle = css`
+  ${emotionReset}
+  body {
+    font-family: "Noto Sans KR", sans-serif;
+    line-height: 1.4;
+    font-size: 16px;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  table {
+    width: 100%;
+  }
+  img {
+    max-width: 100%;
+    vertical-align: top;
+  }
+`;
+
+
 class App extends Component {
   render() {
     return (
       <>
-        <Header></Header>
+        <Global
+          styles={css`
+          ${GlobalStyle}
+        `}
+        />
+        <Header />
         <Router />
       </>
     );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,33 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+
 import React from "react";
+
+import { jsx, css } from "@emotion/react";
+
+const List = css`
+    display: flex;
+    align-items: center;
+    text-align: center;
+
+    > li {
+        flex: 1 1 auto;
+        font-size: 2rem;
+
+        > a {
+            display: block;
+
+            :hover {
+                background-color: #00f;
+                color: #fff;
+            }
+        }
+    }
+`;
 
 export default () => (
     <header>
-        <ul>
+        <ul css={List}>
             <li>
                 <a href="/">Movies</a>
             </li>
