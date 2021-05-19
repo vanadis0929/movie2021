e027be4eb78ade238aab3bbee6651537d4faab38
김병오 <byeongoh.kim@newin.co.kr>
2021-05-19 14:46:20 +0900

    4.3 완료

--- src/Components/api.js
+++ src/Components/api.js
@@ -2,33 +2,47 @@ import { ApiOutlined } from "@ant-design/icons";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.themoviedb.org/3/",
-  params: {
-    api_key: "c7366ad994f02b8aeb39d880756fc500",
-    language: "ko-KR",
-  },
+    baseURL: "https://api.themoviedb.org/3/",
+    params: {
+        api_key: "c7366ad994f02b8aeb39d880756fc500",
+        language: "ko-KR",
+    },
 });
 
 //api.get("tv/popular");
 
 export const moviesApi = {
-  nowPlaying: () => api.get("movie/now_playing"),
-  upcoming: () => api.get("movie/upcoming"),
-  popular: () => api.get("movie/popular"),
-  movieDetail: id => api.get(`movie/${id}`), {
-    params: {
-      append_to_response:  "video, image"
-    }
-  }
+    nowPlaying: () => api.get("movie/now_playing"),
+    upcoming: () => api.get("movie/upcoming"),
+    popular: () => api.get("movie/popular"),
+    movieDetail: (id) =>
+        api.get(`movie/${id}`, {
+            params: {
+                append_to_response: "videos",
+            },
+        }),
+    search: (term) =>
+        api.get("search/movie", {
+            params: {
+                query: encodeURIComponent(term),
+            },
+        }),
 };
 
 export const tvApi = {
-  nowPlaying: () => api.get("tv/nowplaying"),
-  topRated: () => api.get("tv/top_rated"),
-  airingToday: () => api.get("tv/popular"),
- showDetail: id => api.get(`tv/${id}`), {
-    params: {
-      append_to_response:  "image"
-    }
-  }  
+    nowPlaying: () => api.get("tv/nowplaying"),
+    topRated: () => api.get("tv/top_rated"),
+    airingToday: () => api.get("tv/popular"),
+    showDetail: (id) =>
+        api.get(`tv/${id}`, {
+            params: {
+                append_to_response: "videos",
+            },
+        }),
+    search: (term) =>
+        api.get("search/tv", {
+            params: {
+                query: encodeURIComponent(term),
+            },
+        }),
 };
