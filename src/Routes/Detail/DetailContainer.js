c32e6f37b3d2842445509765f04fec09820acf53
김병오 <byeongoh.kim@newin.co.kr>
2021-06-09 17:33:35 +0900

    detail presenter 작업중 (movie result가 null인문제 있음)

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -3,53 +3,51 @@ import React from "react";
 import DetailPresenter from "./DetailPresenter";
 
 export default class extends React.Component {
-    constructor(props) {
-        super(props);
-        const {
-            location: { pathname },
-        } = props;
-        this.state = {
-            result: null,
-            error: null,
-            loading: true,
-            isMovie: pathname.includes("/movie/"),
-        };
+  constructor(props) {
+    super(props);
+    const {
+      location: { pathname },
+    } = props;
+    this.state = {
+      result: null,
+      error: null,
+      loading: true,
+      isMovie: pathname.includes("/movie/"),
+    };
+  }
+
+  async componentDidMount() {
+    const {
+      match: {
+        params: { id },
+      },
+      history: { push },
+    } = this.props;
+
+    const { isMovie } = this.state;
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      return push("/");
     }
 
-    async componentDidMount() {
-        const {
-            match: {
-                params: { id },
-            },
-            history: { push },
-        } = this.props;
+    let result = null;
 
-        const { isMovie } = this.state;
-        const parsedId = parseInt(id);
-        if (isNaN(parsedId)) {
-            return push("/");
-        }
-
-        let result = null;
-
-        try {
-            if (isMovie) {
-                ({ data: result } = await movieApi.movieDetail(parsedId));
-            } else {
-                ({ data: result } = await tvApi.showDetail(parsedId));
-            }
-        } catch {
-            this.setState({ error: "아무것도 찾지 못하였습니다." });
-        } finally {
-            this.setState({ loading: false, result });
-        }
+    try {
+      if (isMovie) {
+        ({ data: result } = await movieApi.movieDetail(parsedId));
+      } else {
+        ({ data: result } = await tvApi.showDetail(parsedId));
+      }
+    } catch {
+      this.setState({ error: "아무것도 찾지 못하였습니다." });
+    } finally {
+      this.setState({ loading: false, result });
     }
+  }
 
-    render() {
-        const { result, error, loading } = this.state;
-        console.log(result);
-        return (
-            <DetailPresenter result={result} error={error} loading={loading} />
-        );
-    }
+  render() {
+    const { result, error, loading } = this.state;
+    console.log(result);
+    return <DetailPresenter result={result} error={error} loading={loading} />;
+  }
 }
--- src/Routes/Detail/DetailPresenter.js
+++ src/Routes/Detail/DetailPresenter.js
@@ -1,11 +1,23 @@
 import React from "react";
 import Proptypes from "prop-types";
 import styled from "styled-components";
-import Poster from "Components/Poster";
+
+import Loader from "Components/Loader";
+
+const Container = styled.div`
+  width: 100vw;
+  height: calc(100vh - 41px);
+  background-image: url(${(props) => props.bgImage});
+`;
 
 const DetailPresenter = ({ result, loading, error }) => {
-  console.log({ result, loading, error });
-  return "Detail";
+  console.log({ result });
+  return loading ? (
+    <Loader />
+  ) : (
+    <Container
+      bgImage={`https://image.tmdb.org/t/p/original${result.backdrop_path}`}></Container>
+  );
 };
 
 DetailPresenter.propTypes = {
